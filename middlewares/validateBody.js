9355d69e3da69d93d7e4f066a24b3867299619b0
Irina-anat <iriszechka@ukr.net>
2023-08-18 18:40:00 +0300

    fix validate

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,18 +1,29 @@
 const { HttpError } = require("../helpers");
 
-const validateBody = schema => (req, _, next) => {
-  if (Object.keys(req.body).length === 0) {
-    const error = new HttpError(400, "missing fields");
-    return next(error);
-  }
-  const { error } = schema.validate(req.body);
-  if (error) {
-    next(new HttpError(400, error.message));
-  }
-  next();
-};
+ const validateBody = schema => {
+   const func = (req, _, next) => {
+     if (Object.keys(req.body).length === 0) {
+       const error = new Error("missing fields");
+       error.status = 400;
+       return next(error);
+     }
+     const { error } = schema.validate(req.body);
+     if (error) {
+       next(HttpError(400, error.message));
+     }
+     next();
+   };
+   return func;
+ };
 
-module.exports = validateBody;
+ module.exports = validateBody; 
+
+
+
+
+
+
+
 
 
 
@@ -31,21 +42,3 @@ const validateBody = schema => {
 
 module.exports = validateBody; */
 
-/* const validateBody = (schema) => {
-  const func = (req, _, next) => {
-    if (Object.keys(req.body).length === 0) {
-      const error = new Error("missing fields");
-      error.status = 400;
-      return next(error);
-    }
-    const { error } = schema.validate(req.body);
-    if (error) {
-      next(HttpError(400, error.message));
-    }
-    next();
-  };
-
-  return func;
-}; */
-
-module.exports = validateBody;
