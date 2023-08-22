4dd6de4f6dd7d6dcd4feafbaf19d21682d1be6c1
Irina-anat <iriszechka@ukr.net>
2023-08-22 17:28:15 +0300

    add gitignore

--- server.js
+++ server.js
@@ -2,16 +2,28 @@ const mongoose = require("mongoose");
 
 const app = require('./app');
 
-const DB_HOST = "mongodb+srv://IRYNA:41Zj911GTiUx0j4o@cluster0.p9hrd0y.mongodb.net/contacts_book?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
-mongoose.set('strictQuery', true);
 
-mongoose.connect(DB_HOST)
+ mongoose.connect(DB_HOST)
   .then(() => {
- app.listen(3000) 
-})
+    app.listen(3000)
+  })
   .catch(error => {
     console.log(error.message);
     process.exit(1);
-})
+  }); 
+
+
+
+
+
+
+
+  /* mongoose.set('strictQuery', true);
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Data base connect sucsess"))
+.catch(error => console.log(error.message)) */
+
+
 
